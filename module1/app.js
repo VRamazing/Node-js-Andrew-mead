ab7996df8437df9de556d0d7092419d8ecc716e2
Veer Mahadevan <r.vignesh149@gmail.com>
2024-01-07 20:48:27 +0530

    Added package json and updated codebase

--- module1/app.js
+++ module1/app.js
@@ -1,3 +1,7 @@
+const {name, add} = require("./utils.js");
+const getNotes = require("./notes.js");
+const validator = require("validator")
+
 const fs = require("fs");
 fs.writeFileSync(
     'notes.txt',
@@ -8,4 +12,11 @@ fs.appendFileSync(
     'notes.txt',
     'lets go'
 )
+
+console.log("Writing completed");
+console.log(name);
+console.log(add(4,2));
+console.log(getNotes());
+console.log(validator.isEmail('foo.com'));
+console.log(validator.isURL('http://foo.in'));
 
---
+++ module1/utils.js
@@ -0,0 +1,12 @@
+console.log("utils.js")
+
+const name = "Veer";
+
+const add = function(a,b){
+    return a + b;
+}
+
+module.exports = {
+    name,
+    add
+};
