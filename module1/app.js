d7c4d5344b66b6bde66e4b01ec89a34ade73ce46
Veer Mahadevan <r.vignesh149@gmail.com>
2024-01-07 22:27:38 +0530

    Updated app.js

--- module1/app.js
+++ module1/app.js
@@ -1,8 +1,11 @@
 const {name, add} = require("./utils.js");
 const getNotes = require("./notes.js");
-const validator = require("validator")
-
+const validator = require("validator");
 const fs = require("fs");
+const log = console.log;
+
+const chalk  = require('chalk');
+
 fs.writeFileSync(
     'notes.txt',
     'hi node js'
@@ -12,6 +15,8 @@ fs.appendFileSync(
     'notes.txt',
     'lets go'
 )
+
+log(chalk.green.bold.bgWhite.inverse('Chalk success!'));
 
 console.log("Writing completed");
 console.log(name);
