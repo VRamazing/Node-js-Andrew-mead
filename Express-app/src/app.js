ee5f7c451ef0967103016254a2dbeb5896e234c1
Vignesh Ramesh <r.vignesh149@gmail.com>
2024-01-17 02:43:11 +0530

    eXPRESS APP

---
+++ Express-app/src/app.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
+
+const app = express();
+
+const port = 3000;
+
+const publicPath = path.join(__dirname, "../public")
+const viewsPath = path.join(__dirname, "../templates/views")
+const partialsPath = path.join(__dirname, "../templates/partials")
+
+
+app.use(express.static(publicPath));
+
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+
+hbs.registerPartials(partialsPath);
+
+app.get("/", (req, res) => {
+    res.render("index", {
+        title: "Weather app", 
+        name: "Veer M"
+    });
+})
+
+
+app.get("/about", (req, res) => {
+    res.render("about", {
+        title: "About Me", 
+        name: "Veer M"
+    });
+})
+
+app.get("/help", (req, res) => {
+    res.render("help", {
+        help: "Sry we cant help",
+        title: "Help", 
+        name: "Veer M"
+    });
+})
+
+
+
+app.get("/weather", (req, res) => {
+    res.send({forcast: "Its going to rain", temp: "50"});
+})
+
+app.listen(port, undefined, () => {console.log("Server listening at port:" + port)});
--- Weather-app/utils/forecasts.js
+++ Weather-app/utils/forecasts.js
@@ -1,15 +1,5 @@
 const request = require("request");
 
-// Goal: Create a reusable function for getting the forecast
-//
-// 1. Setup the "forecast" function in utils/forecast.js
-// 2. Require the function in app.js and call it as shown below
-// 3. The forecast function should have three potential calls to callback:
-//    - Low level error, pass string for error
-//    - Coordinate error, pass string for error
-//    - Success, pass forecast string for data (same format as from before)
-
-
 module.exports = function forecast(location, callback){
     request({url: `http://api.weatherstack.com/current?access_key=65650ae6a77f12583d709123dc8bbd25&query=${location}`, json: true}, function (error, response, body) {
         if(error){
