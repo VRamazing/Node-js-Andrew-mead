bd99798a780a2cffa819c4ce117878a889e35a8c
Vignesh Ramesh <r.vignesh149@gmail.com>
2024-01-28 16:50:49 +0530

    USer model updated

--- mongo-app/src/db/mongoose.js
+++ mongo-app/src/db/mongoose.js
@@ -1,42 +1,19 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 mongoose.connect("mongodb://localhost:27017/task-manager-api", {
     useNewUrlParser: true,
     useCreateIndex: true
 })
 
-const User = mongoose.model("User", {
-    name: {
-        type: String
-    },
-    age: {
-        type: Number
-    }
-})
-
-// const me = new User({
-//     name: "Vignesh",
-//     age: 29
-// })
-
-// me.save().then(data=>{
-//     console.log(data)
-// }).catch((error)=>{
-//     console.log(error)
-// });
-
 const Task = mongoose.model("Task", {
     description: {
-        type: String
+        type: String,
+        trim: true,
+        required: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,  
+        default: false
     }
 })
-
-const task = new Task({
-    description: "Exercise",
-    completed: "false"
-})
-
-task.save().then(task=> console.log(task)).catch(err => console.log(err))
