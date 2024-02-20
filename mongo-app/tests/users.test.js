1e0010362f7c211e1daee0a8920e27beea2b0997
Vignesh Ramesh <r.vignesh149@gmail.com>
2024-02-20 18:49:19 +0530

    Test cases finish

--- mongo-app/tests/users.test.js
+++ mongo-app/tests/users.test.js
@@ -1,134 +1,108 @@
-const request = require("supertest");
-const app = require("../src/app");
-const User = require("../src/models/user")
-const jwt = require("jsonwebtoken")
-const mongoose = require("mongoose")
+const request = require('supertest')
+const app = require('../src/app')
+const User = require('../src/models/user')
+const { userOneId, userOne, setupDatabase } = require('./fixtures/db')
 
+beforeEach(setupDatabase)
 
-const userOneId = new mongoose.Types.ObjectId()
-const userOne =  {
-    _id: userOneId,
-    name: "moti man",
-    email: "motievee@example.com",
-    password: "Maypass123",
-    tokens: [{
-        token: jwt.sign({_id: userOneId}, process.env.JWT_SECRET)
-    }]
-}
-
-beforeEach(async() => {
-    await User.deleteMany()
-    await new User(userOne).save()
-})
-
-test("Should signup a new user", async () => {
-    const response = await request(app).post("/users").send({
-        name: "moti veer",
-        email: "motievee@gmail.com",
-        password: "Maypass123"
+test('Should signup a new user', async () => {
+    const response = await request(app).post('/users').send({
+        name: 'Andrew',
+        email: 'andrew@example.com',
+        password: 'MyPass777!'
     }).expect(201)
 
+    // Assert that the database was changed correctly
     const user = await User.findById(response.body.user._id)
     expect(user).not.toBeNull()
 
-    // Assertions about response 
+    // Assertions about the response
     expect(response.body).toMatchObject({
         user: {
-            name: "moti veer",
-            email: "motievee@gmail.com",
-        }, 
+            name: 'Andrew',
+            email: 'andrew@example.com'
+        },
         token: user.tokens[0].token
     })
-    // Password stored in hash check
-    expect(user.password).not.toBe("Maypass123")
-
+    expect(user.password).not.toBe('MyPass777!')
 })
 
-test("Should login existing user", async () => {
-    const response = await request(app).post("/users/login").send({
+test('Should login existing user', async () => {
+    const response = await request(app).post('/users/login').send({
         email: userOne.email,
         password: userOne.password
     }).expect(200)
-
-
-    const user = await User.findById(userOne._id)
+    const user = await User.findById(userOneId)
     expect(response.body.token).toBe(user.tokens[1].token)
 })
 
-test("Should fail login on wrong credentials", async () => {
-    await request(app).post("/users/login").send({
+test('Should not login nonexistent user', async () => {
+    await request(app).post('/users/login').send({
         email: userOne.email,
-        password: "wrongpassword"
+        password: 'thisisnotmypass'
     }).expect(400)
 })
 
-
-test("Should succeed fetching current user details with credentials", async () => {
+test('Should get profile for user', async () => {
     await request(app)
-        .get("/users/me")
-        .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
-        .send().expect(200)
+        .get('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200)
 })
 
-
-
-test("Should succeed fetching current user details with credentials", async () => {
-    await request(app).get("/users/me").send().expect(401)
+test('Should not get profile for unauthenticated user', async () => {
+    await request(app)
+        .get('/users/me')
+        .send()
+        .expect(401)
 })
 
-
-
-test("Should succeed deleting current user when authenticated", async () => {
-    const response = await request(app)
-        .delete("/users/me")
-        .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
-        .send().expect(200)
-
-        
-    const user = await User.findById(userOne._id)
+test('Should delete account for user', async () => {
+    await request(app)
+        .delete('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200)
+    const user = await User.findById(userOneId)
     expect(user).toBeNull()
 })
 
-
-
-test("Should fail deleting current user when authenticated", async () => {
-    await request(app).delete("/users/me").send().expect(401)
+test('Should not delete account for unauthenticate user', async () => {
+    await request(app)
+        .delete('/users/me')
+        .send()
+        .expect(401)
 })
 
-
-test("Should upload avatar image", async () => {
+test('Should upload avatar image', async () => {
     await request(app)
-        .post("/users/me/avatar")
-        .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
-        .attach("avatar", "tests/fixtures/profile-pic.jpg")
+        .post('/users/me/avatar')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .attach('avatar', 'tests/fixtures/profile-pic.jpg')
         .expect(200)
-
     const user = await User.findById(userOneId)
     expect(user.avatar).toEqual(expect.any(Buffer))
 })
 
-
-
-test("Should update valid user field", async () => {
+test('Should update valid user fields', async () => {
     await request(app)
-        .patch("/users/me")
-        .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+        .patch('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
         .send({
-            "name": "Veer",
+            name: 'Jess'
         })
         .expect(200)
-
     const user = await User.findById(userOneId)
-    expect(user.name).toEqual("Veer")
+    expect(user.name).toEqual('Jess')
 })
 
-
-test("Should not update invalid user field", async () => {
+test('Should not update invalid user fields', async () => {
     await request(app)
-        .patch("/users/me")
-        .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+        .patch('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
         .send({
-            "favColor": "Red",
+            location: 'Philadelphia'
         })
         .expect(406)
 })
